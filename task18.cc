2065c9998c5b80ccc1bf68e96d95b5abf3408740
armilx <armilxgamelolend@gmail.com>
2025-05-15 12:27:58 +0300

    Add class Cell

--- task18.cc
+++ task18.cc
@@ -7,3 +7,11 @@
 #include <iomanip>
 
 using namespace std;
+
+// Клас який описує координати клітинки 
+class Cell { 
+public:
+    int row, col;
+    // Конструктор створює клітинку за заданими координатами 
+    Cell(int r = 0, int c = 0) : row(r), col(c) {}
+};
